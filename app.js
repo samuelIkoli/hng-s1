491784cd2573d46b9b4e2dd11980277c35801d24
Samuel Ikoli <98848723+samuelIkoli@users.noreply.github.com>
2022-10-28 08:31:37 +0100

    commissioner

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({
+        slackUsername: 'Ayibanimi Ikoli',
+        backend: true,
+        age: 25,
+        bio: "Ikoli Samuel is the name, Nile university, E.E.E. B.eng, 2021. Want to break into tech and I believe this is a great oppurtunity. Funny af, cute too, sexy too. Grade me A please. (pouting emoji, cute face emoji)"
+    })
+})
+
+app.listen(3000)
