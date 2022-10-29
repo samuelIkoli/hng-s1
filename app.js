7c3dae76760ab767c55e1a21fb89e0f1623bc74c
Samuel Ikoli <samuelikoli@pop-os.localdomain>
2022-10-29 16:31:25 +0100

    commissioner

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
 const express = require('express');
 const app = express();
 
@@ -10,4 +14,9 @@ app.get('/', (req, res) => {
     })
 })
 
-app.listen(process.env.PORT || 3000)
+const port = process.env.PORT || 3000
+
+app.listen(port, (err) => {
+    if (err) console.log("Error in server setup")
+    console.log('Serving on port 80')
+});
