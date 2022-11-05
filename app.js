ecbe0d550df9c701d695e63e93cad7ee234bd435
Samuel Ikoli <98848723+samuelIkoli@users.noreply.github.com>
2022-11-05 15:55:55 +0100

    commissioner

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.json({
@@ -8,6 +13,44 @@ app.get('/', (req, res) => {
         age: 25,
         bio: "Ikoli Samuel is the name, Nile university, E.E.E. B.eng, 2021. Want to break into tech and I believe this is a great oppurtunity. Funny af, cute too, sexy too. Grade me A please. (pouting emoji, cute face emoji)"
     })
+})
+
+app.get('/index', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'));
+})
+
+app.post('/index', (req, res) => {
+    let { x, y, operation_type } = req.body;
+    var ans = 0
+    console.log('hay')
+    x = parseInt(x)
+    y = parseInt(y)
+    if (!x || !y) {
+        let nums = operation_type.match(/\d+/g);
+        x = parseInt(nums[0])
+        y = parseInt(nums[1])
+        console.log(x, y)
+    }
+
+    try {
+        if (operation_type.includes('/') || operation_type.includes('divide') || operation_type.includes('over')) {
+            ans = x / y
+            operation_type = '/'
+        }
+        else if (operation_type.includes('+') || operation_type.includes('add') || operation_type.includes('plus')) {
+            ans = x + y
+            operation_type = '+'
+        }
+        else if (operation_type.includes('-') || operation_type.includes('minus') || operation_type.includes('subtract')) {
+            ans = x - y
+            operation_type = '-'
+        }
+        else
+            res.send('x has to be an integer, y has to be an integer and the operation type has to be addition, subtraction or division.')
+    } catch (error) {
+        res.send('x has to be an integer, y has to be an integer and the operation type has to be addition, subtraction or division.')
+    }
+    res.json({ slackUsername: 'Samuel Ikoli', operation_type, result: ans })
 })
 
 app.listen(3000)
